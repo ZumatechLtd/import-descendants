679f23d2550a62ea6371cfb1c22e48d15aea1f80
Francis Devereux <francis@bright-interactive.co.uk>
2013-06-04 16:22:51 +0100

    Initial commit

---
+++ testsettings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'import_descendants',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 's3=ik&amp;(uq$!l%r$4pz=6v%4!a6_bg8g_()frl&amp;4((ayxmf1d+t'
